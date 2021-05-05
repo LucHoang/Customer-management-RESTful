d10b56aebec6225222878dd5f9a1f72b2d5dbe78
LucHoang <hoangsyluc@gmail.com>
2021-05-05 21:43:19 +0700

    Quản lý khách hàng sử dụng RESTful

---
+++ src/main/java/com/hsl/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package com.hsl.repository;
+
+import com.hsl.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
